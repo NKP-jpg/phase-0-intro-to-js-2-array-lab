6eb73676aaba3cf4bcc983f6a5e4b1fa7bee34ab
Nija <nija.perry21@gmail.com>
2021-03-23 08:37:09 -0400

    commit tree bf910d855665c62b6d490fc67392c61112209ef3

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  name = cats.push("Ralph");
+}
+function destructivelyPrependCat(name) {
+  name = cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+let copyCats = [];
+function appendCat(name) {
+  name = [...cats.push("Broom")];
+  copyCats = [...cats.slice()];
+}
